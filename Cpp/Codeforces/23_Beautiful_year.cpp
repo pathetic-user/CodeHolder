319d03843e5bcd9e83551e9312c144133ba89f8b
pathetic-user <sheikhti1205@outlook.com>
2025-09-19 22:58:59 +0600

    Add problem numbers to filenames for Codeforces solutions to match sorted order.

---
+++ Cpp/Codeforces/23_Beautiful_year.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string year;
+    int count;
+    cin >> year;
+    for(char ch : year){
+        count = ch - 48;
+        if(ch)
+    } cout << count;
+}
